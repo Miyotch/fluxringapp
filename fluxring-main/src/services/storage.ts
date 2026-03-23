fb59b4b9d3881768c6b48ab333df23d4c789262f
Miyotch <smiyosawa@sparklevision.co.jp>
2026-03-23 22:20:39 +0900

    Delete fluxring-main directory

--- fluxring-main/src/services/storage.ts
+++
@@ -1,9 +0,0 @@
-import storage from '@react-native-firebase/storage';
-
-export async function getAudioUrl(path: string): Promise<string> {
-  return storage().ref(path).getDownloadURL();
-}
-
-export async function getArtworkUrl(path: string): Promise<string> {
-  return storage().ref(path).getDownloadURL();
-}
